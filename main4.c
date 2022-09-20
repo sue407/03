a0d435fc990c29205a56b8f9081d3fdc25d7b1dc
sue407 <sue407407@gmail.com>
2022-09-20 17:37:53 +0900

    (4)
    9/20 변수 실습 (4)

---
+++ main4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int  a, b;
+	
+	printf("분자를 입력하세요  : ");
+	scanf("%d", &a);
+	
+	printf("분모를 입력하세요  : ");
+	scanf("%d", &b);
+		
+	printf("나누기의 결과는 : %f",(float)a/b);
+	
+	
+	return 0;
+}
